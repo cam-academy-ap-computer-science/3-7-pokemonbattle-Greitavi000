07e79a828dfcab0fd0fe0abf51dcc84e46d4d82c
greitavi000 <greitavi000@CAM-LAB-21.bgsd.k12.wa.us>
2019-11-01 07:50:14 -0700

    FirstDay.java

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,22 @@
+/* Main() battleStart(); then damage();  statsTable();. 
+ * battleStart();I must have information that another Pokemon is coming, the question is in the name of our 
+ *         Pokemon, what kind of Pokemon we have chosen, and the beginning of the game.
+ * damage();should include another Pokemon attack, Also the question is what with our Pokemon, level, 
+ *         attack, defense, base \ base, STAB, HP, the decision how many of our Pokemon lost points, and how 
+ *         much HP he has left.
+ *statsTable();or another summary. It should have the name of the Pokemon, its level, then the dashed line,
+ * followed by HP, attack, defense, cn attacks, cn protection, speed, then the dashed line again, and
+ *  the learned movements behind it.
+ *  
+ *  */
 
 public class PokemonBattle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("hi");
+		
+		
 	}
 
 }
