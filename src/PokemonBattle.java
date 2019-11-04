95606022686e90a9262c02ab1e88a62e173430e5
greitavi000 <greitavi000@CAM-LAB-21.bgsd.k12.wa.us>
2019-11-04 07:52:48 -0800

    dayTwo.Java

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 /* Main() battleStart(); then damage();  statsTable();. 
  * battleStart();I must have information that another Pokemon is coming, the question is in the name of our 
  *         Pokemon, what kind of Pokemon we have chosen, and the beginning of the game.
- * damage();should include another Pokemon attack, Also the question is what with our Pokemon, level, 
+ * damage(); should include another Pokemon attack, Also the question is what with our Pokemon, level, 
  *         attack, defense, base \ base, STAB, HP, the decision how many of our Pokemon lost points, and how 
  *         much HP he has left.
- *statsTable();or another summary. It should have the name of the Pokemon, its level, then the dashed line,
- * followed by HP, attack, defense, cn attacks, cn protection, speed, then the dashed line again, and
- *  the learned movements behind it.
+ *statsTable(); It should have the name of the Pokemon, its level, then the dashed line,
+ *         followed by HP, attack, defense, Sp. attacks, Sp.protection, speed, then the dashed line again, and
+ *         the learned movements behind it.
  *  
  *  */
 
@@ -14,9 +16,42 @@ public class PokemonBattle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi");
-		
-		
-	}
+		battleStart();
+		damage();
 
+	}
+	public static String battleStart() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Another trainer is issuing a challenge!");
+		System.out.println();
+		System.out.println("Zebstrika appeared.");
+		System.out.println();
+		System.out.println("Which Pokemon do you choose?");
+		System.out.println();
+		String username = userInput.next();
+		System.out.println("You chose " + username + " !");
+		System.out.println();
+		System.out.println("It’s a Pokemon battle between " + username + " and Zebstrika!  Go!");
+		return username;
+	}
+	
+	public static double damage() {
+		double level = ;
+		double stepOne = 2* level + 10;
+		double stepTwo = stepOne/250;
+		double attack=;
+		double stepThree = stepTwo + (attack/diffense);
+		double stepFour =stepThree * base + 2 ; 
+		double random = 0.85 + (math.random() * 0.15) ;
+		double STAB = ;
+		double modifier = random * STAB;
+		
+		
+	System.out.println("Zebstrika used" + username + "Thunderbolt!");
+	System.out.println("Trainer, what are your  stats?");
+	
+	return demage;
+	
+	
+	}
 }
